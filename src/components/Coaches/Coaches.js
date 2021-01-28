2b6a4364c9307ba07d1b37c7c4696858296f0139
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2021-01-28 23:30:39 +0100

    added images, data object and mapped it

--- src/components/Coaches/Coaches.js
+++ src/components/Coaches/Coaches.js
@@ -5,6 +5,111 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
+const coachesData = [
+    {
+        name : "Anna Hebbeln",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image : "/images/Anna Hebbeln.jpeg"
+    },
+    {
+        name : "Bärbel Engelmann",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image: "/images/Bärbel Engelmann.jpeg"
+    },
+    {
+        name : "Birgit Nüchter",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image: "/images/Birgit Nüchter.jpeg"
+    },
+    {
+        name : "Christina Richter I",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image : "/images/Christina Richter I.jpg"
+    },
+    {
+        name : "Claudia Reinhardt",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image: "/images/Claudia Reinhardt.jpeg"
+    },
+    {
+        name : "Claudia Schmidtke",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image: "/images/Claudia Schmidtke.jpeg"
+    },
+    {
+        name : "Cornelia Lütge",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image : "/images/Cornelia Lütge.jpeg"
+    },
+    {
+        name : "Dr. Anja Schäfer",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image: "/images/Dr. Anja Schäfer.jpeg"
+    },
+    {
+        name : "Dr. Anna Perlina",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image: "/images/Dr. Anna Perlina.jpeg"
+    },
+    {
+        name : "Dr. Christine von der Heide",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image: "/images/Dr. Christine von der Heide.jpeg"
+    },
+    {
+        name : "Dr. Sabrina Hense",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image : "/images/Dr. Sabrina Hense.jpeg"
+    },
+    {
+        name : "Dr. Sanaz von Elsner",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image: "/images/Dr. Sanaz von Elsner.jpeg"
+    },
+    {
+        name : "Gabriela Scheinberger",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image: "/images/Gabriela Scheinberger.jpeg"
+    },
+    {
+        name : "Jana Jablonsky",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image : "/images/Jana Jablonsky.jpeg"
+    },
+    {
+        name : "Jules Grant",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image: "/images/Jules Grant.jpeg"
+    },
+    {
+        name : "Martin Friedrich",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image: "/images/Martin Friedrich.jpeg"
+    },
+    {
+        name : "Susanne Schulz",
+        title : "Title and title and also title",
+        text : "Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen",
+        image: "/images/Susanne Schulz.jpeg"
+    }
+]
+
 const CoachDiv = styled.div`
     padding-top : 20px;
     margin-left : 10px;
@@ -37,6 +142,7 @@ class SlickCarousel extends Component {
                 infinite: true,
                 centerPadding: "100px", //controls how much of the next and prev slides are visible
                 speed: 500,
+                swipeToSlide: true,
                 slidesToShow: 4,
             slidesToScroll: 1,
             initialSlide: 0,
@@ -70,16 +176,17 @@ class SlickCarousel extends Component {
         return (
           <div>
             <Slider {...settings}>
-                {[1,2,3,4,5,6,7,8,9,10].map(element => (
-                <CoachDiv>
-                    <Img src={"https://via.placeholder.com/400x250.png?text="+ element} alt=""/>
+                {coachesData.map((coach, index) => (
+                <CoachDiv key={index}>
+                    <Img src={coach.image} alt=""/>
                     <TextDiv>
-                        <h2>Name Lastname</h2>
-                        <p><strong>Role, another role, also role</strong></p>
-                        <p>Bacon ipsum dolor amet short loin short ribs strip steak, bresaola turducken shankle chuck andouille picanha pork loin kevin. Meatball ground round beef ribs drumstick. Swine short loin beef ribs, bacon biltong burgdoggen</p>
+                        <h2>{coach.name}</h2>
+                        <p><strong>{coach.title}</strong></p>
+                        <p>{coach.text}</p>
                     </TextDiv>   
                 </CoachDiv>
                 ))}
+                
             </Slider>
           </div>
         );
