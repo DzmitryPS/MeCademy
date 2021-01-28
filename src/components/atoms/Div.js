9d56e3ed1d5e39e7fa7fcabbd82f70ced21a2afb
Nadine Strobel <69895853+Nadine1111@users.noreply.github.com>
2021-01-28 16:51:47 +0100

    Quote component set up

--- src/components/atoms/Div.js
+++ src/components/atoms/Div.js
@@ -6,7 +6,11 @@ const Div = styled.div`
     background-color : ${props => props.backgroundColor};
     margin-top : ${props => props.marginTop};
     margin-bottom : ${props => props.marginBottom};
-    
+    display: flex;
+    text-align: center;
+    align-items: center;
+    background-color : #5f7375;
+    justify-content: center;
 `
 
 export default Div;
--- src/components/molecules/Quote.js
+++ src/components/molecules/Quote.js
@@ -1,11 +1,24 @@
+import styled from "styled-components";
 import Div from "../atoms/Div";
 import P from "../atoms/P";
 
 
 
 const Quote = (props) => (
-    <Div backgroundColor="grey" height="100px">
-        <P color="blue" fontSize="20px">Find your next job!</P>
+    <Div 
+    height="100px"
+    marginTop="30px"
+    border="5px solid black"
+    display="flex"
+    >
+        <div>
+        <P 
+        color="white" 
+        fontSize="30px"
+        >"Find your next job!"</P>
+        </div>
+          
+
     </Div>
 )
 
